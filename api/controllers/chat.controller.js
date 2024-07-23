a9516e22e6c510d53e97945a165d1426e1e40c6f
kanika <kanikamukati14@gmail.com>
2024-07-23 20:37:10 +0530

    fixed bug in chat button

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -72,6 +72,27 @@ export const getChat = async (req, res) => {
   }
 };
 
+// export const addChat = async (req, res) => {
+//   const tokenUserId = req.userId;
+//   const { receiverId } = req.body;
+
+//   if (!receiverId) {
+//     return res.status(400).json({ message: "Receiver ID is required" });
+//   }
+
+//   try {
+//     const newChat = await prisma.chat.create({
+//       data: {
+//         userIDs: [tokenUserId, receiverId],
+//       },
+//     });
+//     res.status(200).json(newChat);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: "Failed to add chat!" });
+//   }
+// };
+
 export const addChat = async (req, res) => {
   const tokenUserId = req.userId;
   try {
